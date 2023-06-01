510a5ae10fd69403aadeb4266034421000b1f9f0
k3vinrich4rd <kevinrichardve@gmail.com>
2023-05-31 23:23:40 -0300

    Inserindo teste para tratar métodos que lançam uma exception.

--- src/main/java/br/com/alura/tdd/service/BonusService.java
+++ src/main/java/br/com/alura/tdd/service/BonusService.java
@@ -1,19 +1,20 @@
 package br.com.alura.tdd.service;
 
+import br.com.alura.tdd.modelo.Funcionario;
+
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
-import br.com.alura.tdd.modelo.Funcionario;
-
 public class BonusService {
 
-	public BigDecimal calcularBonus(Funcionario funcionario) {
-		BigDecimal valor = funcionario.getSalario().multiply(new BigDecimal("0.1"));
-		if (valor.compareTo(new BigDecimal("1000")) > 0) {
-			valor = BigDecimal.ZERO;
-		}
-		//Para retornar duas casas decimais e definindo a regra de arredondamento
-		return valor.setScale(2, RoundingMode.UP);
-	}
+    public BigDecimal calcularBonus(Funcionario funcionario) {
+        BigDecimal valor = funcionario.getSalario().multiply(new BigDecimal("0.1"));
+        if (valor.compareTo(new BigDecimal("1000")) > 0) {
+            throw new IllegalArgumentException("Funcionário com salário maior do " +
+                    "que R$ 10000, não pode receber bônus");
+        }
+        //Para retornar duas casas decimais e definindo a regra de arredondamento
+        return valor.setScale(2, RoundingMode.UP);
+    }
 
 }
--- src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
+++ src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
@@ -6,23 +6,43 @@ import org.junit.jupiter.api.Test;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import static org.junit.jupiter.api.Assertions.*;
 
 class BonusServiceTest {
 
+    //Testando uma exception
     @Test
     void bonusDeveriaSerZeroParaFuncionarioComsalarioMuitoAlto() {
         BonusService bonusService = new BonusService();
-        BigDecimal bonus = bonusService.calcularBonus(new Funcionario("Kevin", LocalDate.now(), new BigDecimal("25000")));
 
-        assertEquals(new BigDecimal("0.00"), bonus);
+        assertThrows(IllegalArgumentException.class, () ->
+                bonusService.calcularBonus(new Funcionario("Kevin",
+                        LocalDate.now(), new BigDecimal("25000"))));
+
+    }
+
+    //Testando uma exception (Segunda maneira)
+    //Nessa forma é util para verificar se a mensagem está realmente vindo
+    @Test
+    void bonusDeveriaSerZeroParaFuncionarioComsalarioMuitoAlto2() {
+        BonusService bonusService = new BonusService();
+
+        try {
+            bonusService.calcularBonus(new Funcionario("Kevin",
+                    LocalDate.now(), new BigDecimal("25000")));
+            fail("Não lançou a exception");
+        } catch (IllegalArgumentException illegalArgumentException) {
+            //Verificando se a mensagem realmente está sendo retornada
+            assertEquals("Funcionário com salário maior do que R$ 10000, " +
+                    "não pode receber bônus", illegalArgumentException.getMessage());
+        }
     }
 
     @Test
     void bonusDeveriaSerDezPorCentoDoSalario() {
         BonusService bonusService = new BonusService();
-        BigDecimal bonus = bonusService.calcularBonus(new Funcionario("José", LocalDate.now(), new BigDecimal("2500")));
+        BigDecimal bonus = bonusService.calcularBonus(new Funcionario("José", LocalDate.now(),
+                new BigDecimal("2500")));
 
         assertEquals(new BigDecimal("250.00"), bonus);
 
@@ -31,7 +51,8 @@ class BonusServiceTest {
     @Test
     void bonusDeveriaSerExatamenteDezPorCentoDoSalario() {
         BonusService bonusService = new BonusService();
-        BigDecimal bonus = bonusService.calcularBonus(new Funcionario("Carlos", LocalDate.now(), new BigDecimal("10000")));
+        BigDecimal bonus = bonusService.calcularBonus(new Funcionario("Carlos", LocalDate.now(),
+                new BigDecimal("10000")));
 
         assertEquals(new BigDecimal("1000.00"), bonus);
     }
